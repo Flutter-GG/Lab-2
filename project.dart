df08ad36782254bb31bdc78c2dd022774b346233
Mohammed Alsahli <mohammed.saud.alsahli@gmail.com>
2023-07-26 13:59:54 +0300

    add comments

--- project.dart
+++ project.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 void main() {
   print("Enter id: ");
+  /* try/exept will catch the errors if there, in this case it is fromal error */
   try {
     int userInput = int.parse(stdin.readLineSync()!);
     IdCardFunc(userInput);
@@ -13,7 +14,7 @@ void main() {
 IdCardFunc(int userInput) {
   var myName = 'Mohammed Alsahli';
   const IDcard = 1102430251;
-
+/* check if the input of the user is equal to the ID Card*/
   if (userInput == IDcard) {
     print('they are the same');
   } else {
