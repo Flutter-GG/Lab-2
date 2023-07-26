bbcd4d0f2516f2a5f411e43bd78f909e5a614011
unknown <nawaf.alasam@gmail.com>
2023-07-26 14:20:41 +0300

    lab2_submition

---
+++ lab2/bin/lab2.dart
@@ -0,0 +1,18 @@
+
+import 'dart:io';
+
+
+void main() { String name= "Nawaf"; const id = 1105715724  ;  
+
+   print("please enter your id:");
+   int EnterUser = int.parse(stdin.readLineSync()!); 
+
+  if(id == EnterUser){
+    print("welecom Nawaf");ؤ=
+
+  } else { print("wrong ID");
+  }
+}
+
+
+ 
