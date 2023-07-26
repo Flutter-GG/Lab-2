bf90e915fe14945e81c6701045052bd5bcc8ffaf
Mohammed Alsahli <mohammed.saud.alsahli@gmail.com>
2023-07-26 14:17:34 +0300

    new edit

--- project.dart
+++ project.dart
@@ -1,23 +1,25 @@
 import 'dart:io';
 
 void main() {
-  print("Enter id: ");
   /* try/exept will catch the errors if there, in this case it is fromat error */
   try {
-    int userInput = int.parse(stdin.readLineSync()!);
-    IdCardFunc(userInput);
+    print("Enter id: ");
+    int? userInput = int.parse(stdin.readLineSync()!);
+    print("Enter name: ");
+    String? useInputname = stdin.readLineSync();
+    IdCardFunc(userInputID: userInput, userInputName: useInputname);
   } on FormatException {
     print('your input is not a number.');
   }
 }
 
-IdCardFunc(int userInput) {
+IdCardFunc({int? userInputID, String? userInputName}) {
   var name = 'Mohammed Alsahli';
   const cardNumber = 1102430251;
 /* check if the input of the user is equal to the ID Card */
-  if (userInput == cardNumber) {
+  if (userInputID == cardNumber) {
     print('they are the same, welcom $name.');
   } else {
-    print('they are not the same, your input is: $userInput.');
+    print('they are not the same, your input is: $userInputID.');
   }
 }
